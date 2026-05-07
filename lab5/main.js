ad273a5a56b3fe10069d66b8374c2ca82e0c416b
evpalli <evangeliapalli@gmail.com>
2026-05-07 16:12:13 +0200

    Adding a Geocoder component

--- lab5/main.js
+++ lab5/main.js
@@ -1,31 +1,76 @@
 // Definition Rijksdriehoekstelsel (EPSG:28992)
-let res = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105];
-let map = L.map('map-canvas', {
+let res = [
+  3440.64, 1720.32, 860.16, 430.08, 215.04, 107.52, 53.76, 26.88, 13.44, 6.72,
+  3.36, 1.68, 0.84, 0.42, 0.21, 0.105,
+];
+let map = L.map("map-canvas", {
   continuousWorld: true,
-  crs: new L.Proj.CRS('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs', {
-    transformation: L.Transformation(-1, -1, 0, 0),
-    resolutions: res,
-    origin: [-285401.920, 903401.920],
-    bounds: L.bounds([-285401.920, 903401.920], [595401.920, 22598.080])
-  }),
+  crs: new L.Proj.CRS(
+    "EPSG:28992",
+    "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs",
+    {
+      transformation: L.Transformation(-1, -1, 0, 0),
+      resolutions: res,
+      origin: [-285401.92, 903401.92],
+      bounds: L.bounds([-285401.92, 903401.92], [595401.92, 22598.08]),
+    },
+  ),
   layers: [],
-  center: [52.010, 4.36744],
+  center: [52.01, 4.36744],
   zoom: 9,
-
 });
-map.attributionControl.setPrefix('');
+map.attributionControl.setPrefix("");
 
 // BRT - (Base Registry Topography) BaseMap PDOK:
-let options = { maxZoom: 14, attribution: 'Map data: <a href="http://www.pdok.nl">BRT Achtergrondkaart</a>' }
-let basemap_pdok = new L.tileLayer('https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:28992/{z}/{x}/{y}.png', options);
+let options = {
+  maxZoom: 14,
+  attribution:
+    'Map data: <a href="http://www.pdok.nl">BRT Achtergrondkaart</a>',
+};
+let basemap_pdok = new L.tileLayer(
+  "https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:28992/{z}/{x}/{y}.png",
+  options,
+);
 
 basemap_pdok.getAttribution = function () {
   return 'BRT Achtergrondkaart <a href="http://www.kadaster.nl">Kadaster</a>.';
-}
+};
 basemap_pdok.addTo(map);
 
 // To group the base layers (background) and make the ToC widget
 let baseLayers = {
-  "Topographical map": basemap_pdok
+  "Topographical map": basemap_pdok,
 };
 let toc = L.control.layers(baseLayers).addTo(map);
+
+// Register a geocoder to the map app
+register_geocoder = function (mapInstance) {
+  let polygon = null;
+
+  function clear() {
+    if (polygon !== null) {
+      mapInstance.removeLayer(polygon);
+    }
+  }
+
+  var geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false,
+  })
+    .on("markgeocode", function (e) {
+      clear();
+      var bbox = e.geocode.bbox;
+      polygon = L.polygon([
+        bbox.getSouthEast(),
+        bbox.getNorthEast(),
+        bbox.getNorthWest(),
+        bbox.getSouthWest(),
+      ]);
+      mapInstance.addLayer(polygon);
+      mapInstance.fitBounds(polygon.getBounds());
+      setTimeout(clear, 2500);
+    })
+    .addTo(mapInstance);
+  return geocoder;
+};
+
+register_geocoder(map);
